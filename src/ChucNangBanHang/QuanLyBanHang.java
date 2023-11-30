5bf206053d71b908ceaef55f31b3b6b3ce98f8a4
nghianhph46340 <146502380+nghianhph46340@users.noreply.github.com>
2023-11-30 08:25:07 +0700

    Update QuanLyBanHang.java

--- src/ChucNangBanHang/QuanLyBanHang.java
+++ src/ChucNangBanHang/QuanLyBanHang.java
@@ -58,7 +58,28 @@ public class QuanLyBanHang {
        return listHoaDon;
     }
 
+<<<<<<< Updated upstream
     ArrayList<HoaDon> getListHoaDon(){
+=======
+    public ArrayList<HoaDon> getListHoaDon() {
+        listHoaDon.clear();
+        try {
+            String sql = "SELECT ma_hoa_don, ngay_tao, ten_nguoi_tao, trang_thai FROM HoaDon";
+            Connection conn = DBConnect.getConnection();
+            Statement stm = conn.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+            while (rs.next()) {
+                HoaDon hd = new HoaDon();
+                hd.setMaHoaDon(rs.getString("ma_hoa_don"));
+                hd.setNgayTao(rs.getString("ngay_tao"));
+                hd.setTenNV(rs.getString("ten_nguoi_tao"));
+                hd.setTinhTrang(rs.getString("trang_thai"));
+                listHoaDon.add(hd);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+>>>>>>> Stashed changes
         return listHoaDon;
     }
 
@@ -78,6 +99,7 @@ public class QuanLyBanHang {
         return listGioHang;
     }
 
+<<<<<<< Updated upstream
 //    ArrayList<GioHang> listTraGioHang(ArrayList<SanPham> listSp) {
 //        ArrayList<GioHang> listSPGioHang = new ArrayList<>();
 //        GioHang gioHang = new GioHang();
@@ -102,4 +124,42 @@ public class QuanLyBanHang {
 //
 //        return listSPGioHang;
 //    }
+=======
+    public void addSanPham(GioHang gh) {
+        String sql = "insert into GioHang (ma_hoa_don,ma_san_pham,ten_san_pham,so_luong,don_gia,thanh_tien) values (?,?,?,?,?,?)";
+        try {
+            Connection conn = DBConnect.getConnection();
+            PreparedStatement stm = conn.prepareStatement(sql);
+            stm.setString(1, gh.getMaHD());
+            stm.setString(2, gh.getMaSP());
+            stm.setString(3, gh.getTenSp());
+            stm.setInt(4, gh.getSoLuong());
+            stm.setDouble(5, gh.getDonGia());
+            stm.setDouble(6, gh.getThanhTien());
+            stm.executeUpdate();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addHoaDon(HoaDon hd) {
+        String sql = "insert into HoaDon(ma_hoa_don,ngay_tao,ten_nguoi_tao,trang_thai) values(?,?,?,?)";
+        try {
+            Connection conn = DBConnect.getConnection();
+            PreparedStatement stm = conn.prepareStatement(sql);
+            stm.setString(1, hd.getMaHoaDon());
+            stm.setString(2, hd.getNgayTao());
+            stm.setString(3, hd.getTenNV());
+            stm.setString(4, hd.getTinhTrang());
+            stm.executeUpdate();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+
+>>>>>>> Stashed changes
 }
