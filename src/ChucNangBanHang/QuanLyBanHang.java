ebf292bf14e89d72531536b58044b1b2b5d96cf6
CHINHHTPH46334 <146152612+CHINHHTPH46334@users.noreply.github.com>
2023-11-30 10:10:00 +0700

    2

--- src/ChucNangBanHang/QuanLyBanHang.java
+++ src/ChucNangBanHang/QuanLyBanHang.java
@@ -69,36 +69,56 @@ public class QuanLyBanHang {
     public ArrayList<GioHang> getListGH(String maHD) {
         listGioHang.clear();
         try {
-            String sql = "select ma_san_pham,ten_san_pham,so_luong,don_gia,thanh_tien from GioHang where ma_hoa_don = ?";
-            Connection conn = DBConnect.getConnection();
-            PreparedStatement ptm = conn.prepareStatement(sql);
-            ptm.setString(6, maHD);
-            ptm.executeUpdate();
-            conn.close();
+        String sql = "select ma_san_pham,ten_san_pham,so_luong,don_gia,thanh_tien from GioHang where ma_hoa_don = ?";
+        Connection conn = DBConnect.getConnection();
+        PreparedStatement ptm = conn.prepareStatement(sql);
+        ptm.setString(1, maHD);
+        ResultSet rs = ptm.executeQuery();
+
+        try {
+            while (rs.next()) {
+                GioHang gh = new GioHang();
+                gh.setMaSP(rs.getString(1));
+                gh.setTenSp(rs.getString(2));
+                gh.setSoLuong(rs.getInt(3));
+                gh.setDonGia(rs.getDouble(4));
+                gh.setThanhTien(rs.getDouble(5));
+
+                listGioHang.add(gh);
+            }
+        } catch (Exception e) {
+            System.out.println(1);
+            e.printStackTrace();
+        } finally {
             try {
-                Statement stm = conn.createStatement();
-                ResultSet rs = stm.executeQuery(sql);
-
-                while (rs.next()) {
-                    GioHang gh = new GioHang();
-                    gh.setMaSP(rs.getString(1));
-                    gh.setTenSp(rs.getString(2));
-                    gh.setSoLuong(rs.getInt(3));
-                    gh.setDonGia(rs.getDouble(4));
-                    gh.setThanhTien(rs.getDouble(5));
-
-                    listGioHang.add(gh);
+                if (rs != null) {
+                    rs.close();
                 }
-            } catch (Exception e) {
-                System.out.println(1);
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
 
-        } catch (Exception e) {
-            System.out.println(2);
-            e.printStackTrace();
+            try {
+                if (ptm != null) {
+                    ptm.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-        return listGioHang;
+    } catch (Exception e) {
+        System.out.println(2);
+        e.printStackTrace();
+    }
+    return listGioHang;
     }
 
     public void addSanPham(GioHang gh) {
